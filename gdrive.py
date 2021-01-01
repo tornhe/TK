6e6231d14fc6cbf2b2a3367b1af199d190a61437
tornhe <tor.helgheim@gmail.com>
2021-01-01 17:36:09 +0100

    fiksa spelarvalg på første skjerm

--- gdrive.py
+++ gdrive.py
@@ -14,12 +14,6 @@ def load(spelar):
     # client.open for opening a spreadsheet
     sheet = client.open(spelar).sheet1
 
-    #print(sheet.get_all_records())
-
-    #data = sheet.get_all_values()
-    #headers = data.pop(0)
-    #df = pd.DataFrame(data, columns=headers)
-
     return sheet
 
 def data_to_df(sheet):
