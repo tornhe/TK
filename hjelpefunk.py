eb15d0db14809ec8b47d2014cc37e586c8796d3d
skrapelodd <sindre.eikeland@hotmail.no>
2021-03-06 19:05:12 +0100

    Fått inn ein liten tabell, begynt å leike lett med "custom" knappar B-)

--- hjelpefunk.py
+++ hjelpefunk.py
@@ -13,24 +13,17 @@ def sjekk_float(x):
 def utbetpros(df, sheets, idx):
     siste_rad_kamp = 0
     siste_rad_resultat = 0
-    '''for i in range(len(df)):
-        if df[i][df[i]['Dato'] == "None"].index[0] > siste_rad_kamp:
-            siste_rad_kamp = df[i][df[i]['Dato'] == "None"].index[0]
-        if df[i][df[i]['Bet inn?'] == "None"].index[0] > siste_rad_resultat:
-            siste_rad_resultat = df[i][df[i]['Bet inn?'] == "None"].index[0]
-    '''
+
     siste_rad_kamp = df[idx][df[idx]['Dato'] == "None"].index[0]
     siste_rad_resultat = df[idx][df[idx]['Bet inn?'] == "None"].index[0]
     prosent = []
     temp_tuple = ()
     for i in range(0, siste_rad_resultat):
         val = sheets[idx].cell(i+2, 13).value
-        print(val)
         val = float(val.replace(',','.'))
         val = int(val)
         temp_tuple = (i+1, val)
         prosent.append((temp_tuple),)
-    print(prosent)
     return prosent
 
 
